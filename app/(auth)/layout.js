ab8592f3b007bbd24e64246092608e87828889d0
Mayank Gaur <mayankgaur1504@gmail.com>
2025-07-18 11:33:30 +0530

    Fixed the alignment issues of (auth) pages

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,7 +1,7 @@
 
 /* this authlayout will apply to both signup and sigin pages */
 const AuthLayout = ({ children }) => {
-  return <div className="flex justify-center pt-30 pb-10">{children}</div>;
+  return <div className="flex justify-center pt-28 pb-10">{children}</div>;
 };
 
 export default AuthLayout;
